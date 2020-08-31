76a97d50637da056910bb19895b76ae8c5a36ae1
Rebekah Salem-Dalgety <rebekahsalem-dalgety@Rebekahs-MacBook-Pro.local>
2020-08-31 16:18:54 +0100

    First version of guessing game

---
+++ precourse_recap.py
@@ -0,0 +1,9 @@
+guess_my_number = input("Pick my favourite number between 1 and 5 =  ")
+if  guess_my_number == str("3"):
+     print("Yay, you guessed my favourite number!!! ")
+elif guess_my_number == str("4"):
+    print("Nooooooo, this is the number of death! ")
+elif guess_my_number == str(1):
+    print("Well, that's a bit on the nose, really isn't it. ")
+else:
+    print("Just no... why would you think that? ")
